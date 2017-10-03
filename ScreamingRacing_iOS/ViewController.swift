b3c9c4cd15f5379976628962e9523ac981a882be
ChangChao-Tang <mrgmp2004@hotmail.com>
2017-10-03 15:40:51 +0800

    audio meter demo

--- ScreamingRacing_iOS/ViewController.swift
+++ ScreamingRacing_iOS/ViewController.swift
@@ -7,19 +7,129 @@
 //
 
 import UIKit
+import AVFoundation
+import PinLayout
 
 class ViewController: UIViewController {
-
+    let docUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+    let recBtn = UIButton()
+    let powLabel = UILabel()
+    let recSession = AVAudioSession.sharedInstance()
+    var recorder:AVAudioRecorder?
+    let meterQueue = OperationQueue()
+    var filePath:URL?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupUI()
+        setupSession()
+        meterQueue.qualityOfService = .userInitiated
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupSession() {
+        do {
+            try recSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try recSession.setActive(true)
+            recSession.requestRecordPermission({ [unowned self] (allowed) in
+                DispatchQueue.main.async {
+                    if !allowed {
+                        self.recBtn.isEnabled = false
+                        print("session not allowed")
+                    }
+                }
+            })
+        }catch{
+            
+        }
     }
+    
+    func setupUI() {
+        recBtn.layer.borderWidth = 1
+        recBtn.layer.borderColor = UIColor.black.cgColor
+        recBtn.layer.cornerRadius = 10
+        recBtn.setTitle("Record", for: .normal)
+        recBtn.setTitleColor(UIColor.black, for: .normal)
+        recBtn.setTitleColor(UIColor.gray, for: .highlighted)
+        recBtn.setTitleColor(UIColor.lightGray, for: .disabled)
+        view.addSubview(recBtn)
+        recBtn.addTarget(self, action: #selector(onRecord(sender:)), for: .touchUpInside)
+        
+        powLabel.text = "0"
+        powLabel.font = UIFont.systemFont(ofSize: 20)
+        powLabel.textColor = UIColor.black
+        powLabel.textAlignment = .center
+        view.addSubview(powLabel)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        recBtn.pin.size(CGSize(width: 100, height: 30)).marginBottom(20).bottomCenter(to: view.anchor.bottomCenter)
+        powLabel.pin.size(CGSize(width: 200, height: 200)).marginTop(60).hCenter()
+    }
+    
+    @objc func onRecord(sender:UIButton) {
+        if recorder == nil {
+            startRecord()
+        }else {
+            finishRecording(successed: true)
+        }
+    }
+    
+    func startRecord() {
+        let fileName = UUID().uuidString
+        filePath = docUrl?.appendingPathComponent(fileName).appendingPathExtension("m4a")
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        do {
+            recorder = try AVAudioRecorder(url: filePath!, settings: settings)
+            recorder?.delegate = self
+            recorder?.record()
+            recorder?.isMeteringEnabled = true
+            recBtn.setTitle("Stop", for: .normal)
+        }catch {
+            finishRecording(successed: false)
+            return
+        }
+        let opr = BlockOperation()
+        opr.addExecutionBlock { [unowned self] () in
+            while(!opr.isCancelled){
+                guard let recorder = self.recorder else {
+                    return
+                }
+                recorder.updateMeters()
+                let avgPow = recorder.averagePower(forChannel: 0)
+                let peakPow = recorder.peakPower(forChannel: 0)
+                DispatchQueue.main.async {
+                    self.onUpdatePower(peak: peakPow, avg: avgPow)
+                }
+                usleep(500)
+            }
+        }
+        meterQueue.addOperation(opr)
+    }
+    
+    func finishRecording(successed:Bool) {
+        meterQueue.cancelAllOperations()
+        recorder?.stop()
+        recorder = nil
+        recBtn.setTitle("Record", for: .normal)
+        do {
+            try FileManager.default.removeItem(at: filePath!)
+        }catch {
+            print(error)
+        }
+    }
+    
+    func onUpdatePower(peak:Float, avg:Float) {
+        powLabel.text = String.init(format: "%.2f", avg)
+    }
+}
 
-
+extension ViewController: AVAudioRecorderDelegate {
+    
 }
 
