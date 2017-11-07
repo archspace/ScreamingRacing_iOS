d2b1db87a4bda746a2d24e257f20ba3b81c4bea4
ChangChao-Tang <mrgmp2004@hotmail.com>
2017-11-07 22:46:02 +0800

    speedbar gradient

--- ScreamingRacing_iOS/Controllers/ControlViewController.swift
+++ ScreamingRacing_iOS/Controllers/ControlViewController.swift
@@ -10,17 +10,22 @@ import UIKit
 import PinLayout
 
 class ControlViewController: UIViewController {
-
+    
+    let speedbar = SpeedBarView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
     }
     
     private func setupUI(){
         view.backgroundColor = AppColor.ControllBackground
+        view.addSubview(speedbar)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        speedbar.pin.center().width(250).height(190)
     }
 
 
--- ScreamingRacing_iOS/Views/SpeedBarView.swift
+++ ScreamingRacing_iOS/Views/SpeedBarView.swift
@@ -7,15 +7,45 @@
 //
 
 import UIKit
+import AEConicalGradient
+import ChameleonFramework
+import PinLayout
 
 class SpeedBarView: UIView {
+    
+    let baseGradient = ConicalGradientLayer()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupUI()
     }
-    */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    private func setupUI(){
+        let baseAlpha:CGFloat = 0.25
+        baseGradient.colors = [
+            AppColor.ControllGradient1st!.withAlphaComponent(baseAlpha),
+            AppColor.ControllGradient2nd!.withAlphaComponent(baseAlpha),
+            AppColor.ControllGradient3rd!.withAlphaComponent(baseAlpha),
+            AppColor.ControllGradient4th!.withAlphaComponent(baseAlpha),
+            AppColor.ControllGradient5th!.withAlphaComponent(baseAlpha)
+        ]
+        baseGradient.startAngle = 0.75 * Double.pi
+        baseGradient.endAngle = 2.25 * Double.pi
+        layer.addSublayer(baseGradient)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        baseGradient.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
+    }
+    
+    
+    
+ 
 
 }
