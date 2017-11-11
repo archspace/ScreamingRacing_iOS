bdff1ca7e8620600f7e10cb53d212f10fb43c298
ChangChao-Tang <mrgmp2004@hotmail.com>
2017-11-11 17:13:29 +0800

    draw speedbar

--- ScreamingRacing_iOS/Views/SpeedBarView.swift
+++ ScreamingRacing_iOS/Views/SpeedBarView.swift
@@ -14,6 +14,9 @@ import PinLayout
 class SpeedBarView: UIView {
     
     let baseGradient = ConicalGradientLayer()
+    let baseMask = CAShapeLayer()
+    let gradient = ConicalGradientLayer()
+    let gradientMask = CAShapeLayer()
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -26,7 +29,7 @@ class SpeedBarView: UIView {
     }
     
     private func setupUI(){
-        let baseAlpha:CGFloat = 0.25
+        let baseAlpha:CGFloat = 0.1, startAngle = 0.75 * Double.pi, endAngle = 2.25 * Double.pi
         baseGradient.colors = [
             AppColor.ControllGradient1st!.withAlphaComponent(baseAlpha),
             AppColor.ControllGradient2nd!.withAlphaComponent(baseAlpha),
@@ -34,18 +37,45 @@ class SpeedBarView: UIView {
             AppColor.ControllGradient4th!.withAlphaComponent(baseAlpha),
             AppColor.ControllGradient5th!.withAlphaComponent(baseAlpha)
         ]
-        baseGradient.startAngle = 0.75 * Double.pi
-        baseGradient.endAngle = 2.25 * Double.pi
+        baseGradient.startAngle = startAngle
+        baseGradient.endAngle = endAngle
+        baseGradient.mask = baseMask
         layer.addSublayer(baseGradient)
+        
+        gradient.colors = [
+            AppColor.ControllGradient1st!,
+            AppColor.ControllGradient2nd!,
+            AppColor.ControllGradient3rd!,
+            AppColor.ControllGradient4th!,
+            AppColor.ControllGradient5th!
+        ]
+        gradient.startAngle = startAngle
+        gradient.endAngle = endAngle
+        gradient.mask = gradientMask
+        layer.addSublayer(gradient)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        baseGradient.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
+        baseGradient.frame = bounds
+        gradient.frame = baseGradient.frame
+        baseMask.frame = baseGradient.bounds
+        gradientMask.frame = gradient.bounds
+        let path = UIBezierPath(arcCenter: CGPoint(x:bounds.midX, y:bounds.midY), radius: bounds.height * 0.45, startAngle: 0.75 * CGFloat.pi, endAngle: 2.25 * CGFloat.pi, clockwise: true)
+        gradientMask.path = path.cgPath
+        gradientMask.strokeEnd = 0
+        gradientMask.lineWidth = 5
+        gradientMask.fillColor = UIColor.clear.cgColor
+        gradientMask.strokeColor = UIColor.black.cgColor
+        
+        baseMask.path = UIBezierPath(cgPath: path.cgPath).cgPath
+        baseMask.lineWidth = 5
+        baseMask.fillColor = UIColor.clear.cgColor
+        baseMask.strokeColor = UIColor.black.cgColor
     }
     
-    
-    
- 
+    func setSpeedRate(speed:CGFloat) {
+        gradientMask.strokeEnd = speed
+    }
 
 }
